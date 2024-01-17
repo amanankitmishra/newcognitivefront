a573f1596642af6a7da79b5695b08bc5389abc48
Aman Ankit <amanankitmishra@gmail.com>
2024-01-17 23:21:57 +0530

    eslint

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
